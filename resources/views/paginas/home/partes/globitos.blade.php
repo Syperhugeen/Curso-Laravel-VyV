965d8f583b3ac4d45593ba0f410dd407a8af7805
Eruni えるねすと <eccb.contacto@gmail.com>
2017-05-02 21:14:46 -0300

    pfff
    pedidos que cagan el diseño, prontos

--- resources/views/paginas/home/partes/globitos.blade.php
+++ resources/views/paginas/home/partes/globitos.blade.php
@@ -44,39 +44,39 @@
     <div class="row">
 
     <a href="{{route('get_pagina_empresa')}}">
-    <div class="col-xs-6 col-sm-4 cuadradito">
+    <div class="col-xs-6 col-sm-6 cuadradito">
     <img class="img-responsive" src="imagenes/Img/home-icon-acerca.png"><p>Quienes somos</p>
     </div>
     </a>
 
     <a href="{{route('get_pagina_servicios')}}#section-metodos">
-    <div class="col-xs-6 col-sm-4 cuadradito">
+    <div class="col-xs-6 col-sm-6 cuadradito">
     <img class="img-responsive" src="imagenes/Img/home-icon-metodos.png"><p>Métodos de construcción</p>
     </div>
     </a>
 </div>
 <div class="row">
     <a href="{{route('get_pagina_servicios')}}">
-    <div class="col-xs-6 col-sm-4 cuadradito">
+    <div class="col-xs-6 col-sm-6 cuadradito">
     <img class="img-responsive" src="imagenes/Img/home-icon-servicios.png"><p>Servicios que ofrecemos</p>
     </div>
     </a>
 
     <a href="{{route('get_pagina_proyecto_listado')}}">
-    <div class="col-xs-6 col-sm-4 cuadradito">
+    <div class="col-xs-6 col-sm-6 cuadradito">
     <img class="img-responsive" src="imagenes/Img/home-icon-proyectos.png"><p>Portfolio de proyectos</p>
     </div>
     </a>
 </div>
 <div class="row">
     <a href="{{route('get_pagina_noticias_listado')}}">
-    <div class="col-xs-6 col-sm-4 cuadradito">
+    <div class="col-xs-6 col-sm-6 cuadradito">
     <img class="img-responsive" src="imagenes/Img/home-icon-publicaciones.png"><p>Publicaciones interesantes</p>
     </div>
     </a>
 
     <a href="{{route('get_pagina_contacto')}}">
-    <div class="col-xs-6 col-sm-4 cuadradito">
+    <div class="col-xs-6 col-sm-6 cuadradito">
     <img class="img-responsive" src="imagenes/Img/home-icon-contacto.png"><p>Contáctanos</p>
     </div>
     </a>
