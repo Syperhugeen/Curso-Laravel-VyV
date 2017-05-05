c5d41f89b307db41b892a8710219c374b12fa6c3
Eruni えるねすと <eccb.contacto@gmail.com>
2017-05-05 01:03:45 -0300

    ultimos cambios de la galería de portada
    Actualizar en la página real también por favor. Gracias!

--- resources/views/paginas/home/partes/imagen_de_portada.blade.php
+++ resources/views/paginas/home/partes/imagen_de_portada.blade.php
@@ -1,5 +1,5 @@
 <!--este es el carrusel de imágenes de portada-->
-
+<div class="hidden-xs">
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
 
   <!-- Indicators -->
@@ -41,4 +41,69 @@
     <span class="sr-only">Next</span>
   </a>
 
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--este es el carrusel de imágenes de portada xs-->
+<div class="visible-xs">
+<div id="myCarouselxs" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarouselxs" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarouselxs" data-slide-to="1"></li>
+    <li data-target="#myCarouselxs" data-slide-to="2"></li>
+    <li data-target="#myCarouselxs" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides xs -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img class="img-carousel" src="imagenes/Img/metodo1xs.jpg" alt="Ingeniería Civil">
+    </div>
+
+    <div class="item">
+      <img class="img-carousel" src="imagenes/Img/metodo2xs.jpg" alt="Ingeniería Civil">
+    </div>
+
+    <div class="item">
+      <img class="img-carousel" src="imagenes/Img/metodo3xs.jpg" alt="Ingeniería Civil">
+    </div>
+
+    <div class="item">
+      <img class="img-carousel" src="imagenes/Img/metodo4xs.jpg" alt="Ingeniería Civil">
+    </div>
+  </div>
+
+    <!-- Left and right controls xs-->
+  <a class="left carousel-control" href="#myCarouselxs" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+
+  <a class="right carousel-control" href="#myCarouselxs" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+
+</div>
 </div>
