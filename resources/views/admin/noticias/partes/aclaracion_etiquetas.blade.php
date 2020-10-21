4965d204af4f1a2f600e4e48147877f89577ac9d
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-10-21 13:29:11 -0300

    df

--- resources/views/admin/noticias/partes/aclaracion_etiquetas.blade.php
+++ resources/views/admin/noticias/partes/aclaracion_etiquetas.blade.php
@@ -1,7 +1,7 @@
-<ul class="col-12"> 
-   <li class=""> Bold: <strong> (B)   (/B) </strong> </li> 
-   <li class=""> Italic: <strong > (I)   (/I) </strong> </li> 
-   <li class=""> Links: <strong > (A) Aquí el link (/A) (AT) Aquí el texto (/AT) </strong> 
+<ul class="contenedor-aclaraciones"> 
+   <li class="linea-de-aclaración"> Bold: <strong> (B)   (/B) </strong> </li> 
+   <li class="linea-de-aclaración"> Italic: <strong > (I)   (/I) </strong> </li> 
+   <li class="linea-de-aclaración"> Links: <strong > (A) Aquí el link (/A) (AT) Aquí el texto (/AT) </strong> 
    </li>
 </ul>
 
