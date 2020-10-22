1403eb7bb9060934100bd85ddc22847010531693
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-10-22 12:43:43 -0300

    df

--- resources/views/paginas/home/partes/imagen_de_portada.blade.php
+++ resources/views/paginas/home/partes/imagen_de_portada.blade.php
@@ -2,16 +2,20 @@
 <div class="hidden-xs">
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
 
-  <!-- Indicators -->
+  <!-- Indicators --> 
   <ol class="carousel-indicators">
     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     <li data-target="#myCarousel" data-slide-to="1"></li>
     <li data-target="#myCarousel" data-slide-to="2"></li>
     <li data-target="#myCarousel" data-slide-to="3"></li>
+    <li data-target="#myCarousel" data-slide-to="4"></li>
   </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img class="img-carousel" src="imagenes/Portadas/veigaventos-portada-1.jpg" alt="Ingeniería Civil">
+    </div>
     <div class="item active">
       <img class="img-carousel" src="imagenes/Img/metodo1.jpg" alt="Ingeniería Civil">
     </div>
