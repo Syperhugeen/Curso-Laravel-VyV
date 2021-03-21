ab21da9e274bf82858c3bceca43b51bc7dcc320a
Mauricio <ventas@worldmaster.com.uy>
2021-03-21 16:48:14 -0300

    build para subir a produccion

--- resources/views/paginas/webpack_compilado/index.blade.php
+++ resources/views/paginas/webpack_compilado/index.blade.php
@@ -5,4 +5,4 @@
         window.addEventListener('load', () => {
           navigator.serviceWorker.register('/service-worker.js');
         });
-      }</script><script defer="defer" src="/js/home.9ea0c1f364f8c7f44ee7.js"></script><link href="/css/home.2357f257f08f4c1f68e3.css" rel="stylesheet"></head><body><div id="root"></div><div id="modal-root"></div></body></html>
+      }</script><script defer="defer" src="/js/home.28666254838b4fa5b677.js"></script><link href="/css/home.e860fbdf5ea049fc94c5.css" rel="stylesheet"></head><body><div id="root"></div><div id="modal-root"></div></body></html>
