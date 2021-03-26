59eeabe69b89aa12d752aa64477fa11c47e01993
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-03-26 19:04:53 -0300

    slider simple ajust

--- resources/assets/webpack/js/components/sliderSimple.jsx
+++ resources/assets/webpack/js/components/sliderSimple.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
+import Skeleton from 'react-loading-skeleton';
 import '../../css/components/sliderSimple.scss';
 const SliderSimple = ({ images }) => {
   // takes in images as props
   const [current, setCurrent] = useState(0); // create state to keep track of images index, set the default index to 0
+
+  const [cantidadDeImagenesCargadas, setCantidadDeImagenesCargadas] = useState(
+    0
+  );
+
+  const [loaded, setLoaded] = useState(false);
   const length = images.length;
 
   <i class="far fa-arrow-alt-circle-left"></i>;
@@ -16,29 +23,63 @@ const SliderSimple = ({ images }) => {
     setCurrent(current === 0 ? length - 1 : current - 1);
   };
 
+  useEffect(() => {
+    images.forEach((image) => {
+      let img = new Image();
+      img.src = image;
+
+      const timeOut = setTimeout(function () {
+        setLoaded(true);
+      }, 400);
+
+      return () => {
+        clearTimeout(timeOut);
+      };
+    });
+  }, []);
+
   return (
-    <div className="slider">
-      <button className="slider-simple-before" onClick={slideLeft}>
-        {<FaArrowAltCircleLeft />}
-      </button>
-
-      {images.map((img, index) => {
-        return (
-          <div
-            key={index}
-            className={index === current ? 'slide active' : 'slide'}
-          >
-            {index === current && (
-              <img className="img-fluid" src={img} alt={index} />
-            )}
+    <>
+      {!loaded ? (
+        <div className="container my-3">
+          <div className="w-100 mb-2 ">
+            <Skeleton count={3} />
           </div>
-        );
-      })}
+          <div className="w-100 mb-2 ">
+            <Skeleton count={3} />
+          </div>
+          <div className="w-100 mb-2 ">
+            <Skeleton count={3} />
+          </div>
+          <div className="w-100 mb-2 ">
+            <Skeleton count={3} />
+          </div>
+        </div>
+      ) : (
+        <div className="slider">
+          <button className="slider-simple-before" onClick={slideLeft}>
+            {<FaArrowAltCircleLeft />}
+          </button>
 
-      <button className="slider-simple-next" onClick={slideRight}>
-        {<FaArrowAltCircleRight />}
-      </button>
-    </div>
+          {images.map((img, index) => {
+            return (
+              <div
+                key={index}
+                className={index === current ? 'slide active' : 'slide'}
+              >
+                {index === current && (
+                  <img className="img-fluid" src={img} alt={index} />
+                )}
+              </div>
+            );
+          })}
+
+          <button className="slider-simple-next" onClick={slideRight}>
+            {<FaArrowAltCircleRight />}
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
