13d96c4c9acc3f960ab9c6957e80982635944cfe
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-05-13 16:02:39 -0300

    df

--- app/Managers/envio_contacto_manager.php
+++ app/Managers/envio_contacto_manager.php
@@ -15,6 +15,7 @@ class envio_contacto_manager extends ManagerBase
             'name'    => 'required',
             'email'   => 'required|email',
             'message' => 'required',
+            'file'    => 'mimes:doc,docx,pdf|max:3000',
         ];
 
         return $rules;
